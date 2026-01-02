2a14897f3424b8339a87129401bf6f965234e58a
vivekevdt <vivek.evdt@gmail.com>
2026-01-02 12:02:20 +0530

    changes in routing

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
-import {Routes,Route} from "react-router-dom";
+import {Routes,Route,Navigate} from "react-router-dom";
 import Add from './pages/Add/Add';
 import List from './pages/List/List';
 import Orders from './pages/Orders/Orders';
@@ -18,6 +18,8 @@ const App = () => {
       <div className="app-content">
         <Sidebar/>
         <Routes>
+            <Route path="/" element={<Navigate to="/orders" replace />} />
+
           <Route path="/add" element={<Add></Add>}/>;
           <Route path="/list" element={<List></List>}/>;
           <Route path="/orders" element={<Orders></Orders>}/>;
